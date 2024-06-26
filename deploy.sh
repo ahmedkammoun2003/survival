f2b562ade736532391e4283a7959d43ec144acbe
ahmedkammoun2003 <ahmed.kamoun@ensi-uma.tn>
2024-06-26 18:42:23 +0100

    deploy

--- deploy.sh
+++ deploy.sh
@@ -8,30 +8,19 @@ CHATBOT_DIR=$ROOT_DIR/chatbot
 API_DIR=$ROOT_DIR/api
 
 echo "Installing Node.js dependencies..."
-npm install
+npm install &
 
 echo "Installing client dependencies..."
-pushd $CLIENT_DIR
-npm install
-popd
+(cd $CLIENT_DIR && npm install) &
 
 echo "Installing virtualenv..."
-pip install virtualenv
+pip install virtualenv &
 
 echo "Creating virtual environment and installing Python dependencies..."
-pushd $CHATBOT_DIR
-python -m venv venv
-source venv/bin/activate
-pip install -r requirements.txt
-python app.py
-popd
+(cd $CHATBOT_DIR && python -m venv venv && source venv/bin/activate && pip install -r requirements.txt && python app.py) &
 
 echo "Starting client development server..."
-pushd $CLIENT_DIR
-npm run dev
-popd
+(cd $CLIENT_DIR && npm run dev) &
 
 echo "Starting API server..."
-pushd $API_DIR
-node server.js
-popd
+(cd $API_DIR && node server.js) &
