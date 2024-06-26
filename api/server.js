555673546927616b345babb2ba5a8c6f1bc2cf0b
ahmedkammoun2003 <ahmed.kamoun@ensi-uma.tn>
2024-06-26 18:59:31 +0100

    deploy

--- api/server.js
+++ api/server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const markersRouter = require('./routes/Marker.route.js');
 const zoneRouter = require('./routes/Zone.route.js')
 const cors = require('cors');
+const path=require('path')
 require('dotenv').config();
 console.log(process.env.MONGODB);
 
@@ -11,6 +12,7 @@ console.log(process.env.MONGODB);
 const app = express();
 const PORT = 5000;
 app.use(cors())
+const __dirname = path.resolve();
 
 mongoose.connect(process.env.MONGODB).then(() => {
   console.log('Connected to MongoDB');
@@ -21,6 +23,10 @@ app.use(bodyParser.json());
 
 app.use('/api/markers', markersRouter);
 app.use('/api/polygons', zoneRouter);
+app.use(express.static(path.join(__dirname,'/client/dist')));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname,'/client/dist/index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
